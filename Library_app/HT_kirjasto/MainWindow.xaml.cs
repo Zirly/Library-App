035ca6ee5bea0e971815edfa3afbf9022476ff22
Arttu Rousku <M1484@student.jamk.fi>
2019-05-05 22:44:21 +0300

    datagridtest

--- Library_app/HT_kirjasto/MainWindow.xaml.cs
+++ Library_app/HT_kirjasto/MainWindow.xaml.cs
@@ -47,7 +47,17 @@ namespace HT_kirjasto
                 }
                 myListBox.ItemsSource = myList;
             }
-            
+
+            DataTable dg = Database_class.Get_DataTable(sql);
+            if (dg.Rows.Count >= 0)
+            {
+                List<string> myList = new List<string>();
+                foreach (DataRow row in dg.Rows)
+                {
+                    dataGrid.Columns.Add(row["book_id"].ToString());
+                }
+            }
+
         }
     }
 }
