f1bccb01231dfaeaf8394390d751bcc5beca9ab4
Marketa Sovova <M1482@student.jamk.fi>
2019-05-05 05:55:05 +0300

    added connection to the database

--- kirjasto_projekti/kirjasto/MainWindow.xaml.cs
+++ kirjasto_projekti/kirjasto/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+/* Lisätty Database -luokka, joka hoitaa yhteyden tietokantaan ja hakee dataa sql-kommennolla
+ * Alustavasti näyttää käyttäliitymässä kirjojen nimet
+ * TODO
+ * 1) Toimiiko toisellakin koneella? kirjasto > properties > settings > connection_String - sen polku viittaa paikalliseen koneeseen
+ * 2) Pitäisikö seuraavastai parsata tietokannan dataa omiksi luokiksi? - onko tämä oikea tapa hoitaa tietokannan dataa?
+
 namespace kirjasto
 {
     /// <summary>
@@ -23,6 +30,7 @@ namespace kirjasto
         public MainWindow()
         {
             InitializeComponent();
+            /*
             List<string> myList = new List<string>
             {
                 "Berry",
@@ -34,8 +42,18 @@ namespace kirjasto
                 "Arttu",
                 "Marketa"
             };
-            myListBox.ItemsSource = myList;
-
+            myListBox.ItemsSource = myList; */
+            string sql = "SELECT title FROM book_table";
+            DataTable tbl = Database.Get_DataTable(sql);
+            if (tbl.Rows.Count >= 0)
+            {
+                List<string> bookTitles = new List<string>();
+                foreach (DataRow row in tbl.Rows)
+                {
+                    bookTitles.Add(row["title"].ToString());
+                }
+                myListBox.ItemsSource = bookTitles;
+            }
         }
     }
 }
