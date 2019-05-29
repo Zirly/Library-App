f759ebbcfff8c5a53d5d98ddf28ca284167a4f69
Arttu Rousku <M1484@student.jamk.fi>
2019-05-29 21:59:14 +0300

    View for item addition

--- Library_app/library/MainWindow.xaml.cs
+++ Library_app/library/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Library.View;
 using Library.Model;
 using Library.ViewModel;
 
@@ -44,6 +45,12 @@ namespace Library
             BooksListControl.lstBooks.ItemsSource = Books.BooksList;
             BooksListControl.lstBooks.Items.Refresh();
             BooksListControl.lstBooks.SelectedIndex = 0;
+        }
+
+        private void Item_Add_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ItemAdd itemAdd = new ItemAdd();
+            itemAdd.Show();
         }
     }
 }
