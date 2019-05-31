5580f38419ec6778595fe19c03cdc6a12f8b0ef8
Marketa Sovova <M1482@student.jamk.fi>
2019-05-31 23:13:26 +0300

    BookAdd - genre list

--- Library_app/Library/ViewModel/GenresListViewModel.cs
+++ Library_app/Library/ViewModel/GenresListViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,14 @@ namespace Library.ViewModel
 {
     public class GenresListViewModel
     {
+        public ObservableCollection<string> listaGenre = new ObservableCollection<string>();
+        
+        public GenresListViewModel()
+        {
+            listaGenre.Add("a");
+            listaGenre.Add("b");
+            listaGenre.Add("c");
+        }
+       
     }
 }
